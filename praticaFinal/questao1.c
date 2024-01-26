68438ee8eab3822f6ec8003ad44081ba39e92337
Difierro <gustavo7867grdr@gmail.com>
2024-01-26 14:35:35 -0300

    resolução da pratica final

---
+++ praticaFinal/questao1.c
@@ -0,0 +1,67 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct ingresso{
+    float preco;
+    char local[30];
+    char atracao[40];
+}Ingresso;
+
+void preencherDados(Ingresso * ingresso, int qtdIngressos){
+    int i;
+    for (i = 0; i < qtdIngressos; i++){
+        printf("digite a atracao: ");
+        scanf(" %[^\n]", ingresso[i].atracao);
+        printf("digite o local: ");
+        scanf(" %[^\n]", ingresso[i].local);
+        printf("digite o valor do ingresso: ");
+        scanf(" %f", &ingresso[i].preco);
+    }   
+}
+
+void imprimirDados(Ingresso * ingresso, int qtdIngressos){
+    int i;
+    for (i = 0; i < qtdIngressos; i++){
+        printf("Atracao: %s\n", ingresso[i].atracao);
+        printf("Local: %s\n", ingresso[i].local);
+        printf("Valor do ingresso: R$%.2f\n", ingresso[i].preco);
+        printf("------------------------------------------------------\n");
+    }   
+}
+
+void alterarPreco(Ingresso * ingresso, int qtdIngressos){
+    float novoPreco = 0;
+    int i;
+    for( i = 0; i < qtdIngressos; i++){
+        printf("digite o novo valor do ingresso para %s: ", ingresso[i].atracao);
+        scanf(" %f", &novoPreco);
+        ingresso[i].preco = novoPreco;
+    }
+}
+
+void imprimeMaiorMenorPreco(Ingresso * ingresso, int qtdIngressos){
+    int i, maior = 0, menor = 0;
+    for(i = 1; i < qtdIngressos; i++){
+        if(ingresso[i].preco < ingresso[menor].preco){
+            menor = i;
+        }else if(ingresso[i].preco > ingresso[maior].preco){
+            maior = i;
+        }
+    }
+    printf("maior = %s \t valor: %f \n menor = %s \t valor: %f", ingresso[maior].atracao,ingresso[maior].preco, ingresso[menor].atracao, ingresso[menor].preco);
+}
+
+int main(void){
+    int qtdIngressos = 3;
+    Ingresso * ingresso = malloc(qtdIngressos * sizeof(Ingresso));
+
+    preencherDados(ingresso, qtdIngressos);
+    imprimirDados(ingresso, qtdIngressos);
+    alterarPreco(ingresso, qtdIngressos);
+    imprimeMaiorMenorPreco(ingresso, qtdIngressos);
+    
+    free(ingresso);
+
+    return 0;
+}
