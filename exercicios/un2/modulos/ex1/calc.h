43b0dc42af481133956498e88a471496b28b8fa0
Difierro <gustavo7867grdr@gmail.com>
2024-02-02 15:16:46 -0300

    resolução atividade 1 de file

---
+++ exercicios/un2/modulos/ex1/calc.h
@@ -0,0 +1,8 @@
+/*Função que realiza a soma de 2 numeros*/
+float soma(float * num1, float * num2);
+/*Função que realiza a subtração de 2 numeros*/
+float subtracao(float * num1, float * num2);
+/*funcao que realiza a multiplicação de dois numeros*/
+float multiplicacao(float * num1, float * num2);
+/*funcao que realiza a dvisao de 2 numeros*/
+float divisao(float * num1, float * num2);
